89e7307add225f2e0a7c07c6aafde3b74477dc1e
ClementNjeru <njeruclement36@gmail.com>
2023-02-23 10:18:28 +0300

    adds the navbar and updates the Logo

---
+++ src/Components/Navbar.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function CollapsibleExample() {
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="transparent" variant="">
+      <Container className='container1'>
+        <Navbar.Brand href="#home"><img
+              src="favicon.ico"
+              width="100"
+              height="100"
+              radius="5"
+              className="d-inline-block align-top"
+              alt=" DCC logo"
+            /></Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features" >Home</Nav.Link>
+            <Nav.Link href="#pricing">About Us</Nav.Link>
+            <Nav.Link href="#pricing">Ministries</Nav.Link>
+            <Nav.Link href="#pricing">Prayer</Nav.Link>
+            <Nav.Link href="#pricing">Donate</Nav.Link>
+            <NavDropdown title="More" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Blogs</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Media
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Calender</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav className='navL'>
+            <Nav.Link href="#deets">Media</Nav.Link>
+            <Nav.Link href="#memes">
+            Contact Us
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default CollapsibleExample;
