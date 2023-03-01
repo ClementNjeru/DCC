e1c60f3fbf816c424a19d6a0d58ab064cf4303b6
ClementNjeru <njeruclement36@gmail.com>
2023-03-01 11:17:28 +0300

    creates additional components in the about us page

---
+++ src/Components/Doctrine.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Doctrine() {
+  return (
+    <div>Doctrine</div>
+  )
+}
+
+export default Doctrine
---
+++ src/Components/Leadership.js
@@ -0,0 +1,99 @@
+// import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+function BasicExample() {
+  return (
+    <div className='led'>
+      <div className='one'>
+        <Card style={{ width: '25rem', backgroundColor: 'rgb(247, 241, 235)', borderRadius: '20px' }}>
+          <Card.Img variant="top" src="https://scontent.fnbo1-1.fna.fbcdn.net/v/t1.18169-9/24312629_1559599917418882_8293534430085533919_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=cdbe9c&_nc_ohc=9pTD3SAQS4oAX9pUMQL&_nc_ht=scontent.fnbo1-1.fna&oh=00_AfAropxJ4Sg7lncbQBVP-IKUGdeW8UI_LSY8OYeO-8dtwg&oe=64266520" />
+          <Card.Body>
+            <Card.Title>Arch Bishop Meshack Kihara</Card.Title>
+            <Card.Text>
+              Nakuru Diocese
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <div className='text1'>
+          <h2 className='h1'>All Nakuru Parish Churches</h2>
+          <h5>Arch Bishop</h5>
+          <h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h6>
+        </div>
+      </div>
+
+      <br></br>
+      <div className='one'>
+        <Card style={{ width: '25rem', backgroundColor: 'rgb(247, 241, 235)', borderRadius: '20px' }}>
+          <Card.Img variant="top" src="https://scontent.fnbo1-1.fna.fbcdn.net/v/t31.18172-8/24831473_1559600887418785_4987174456813207101_o.jpg?_nc_cat=108&ccb=1-7&_nc_sid=cdbe9c&_nc_ohc=PARagKmLQewAX8-OTDD&_nc_oc=AQkp2lVWuGFmcJ_APtQ_c0j45OCfqd5WxOU8brnQGXMZ-n3vVsnhnAhla_cRecBA20U&_nc_ht=scontent.fnbo1-1.fna&oh=00_AfC9VOfSVhQyIxa7v0zEEnJ3q1DEuAKfAe8DkJhQfXvprQ&oe=64265103" />
+          <Card.Body>
+            <Card.Title>Bishop Njenga</Card.Title>
+            <Card.Text>
+              Western Diocese
+            </Card.Text>
+          </Card.Body>
+        </Card>
+
+        <div className='text1'>
+          <h2 className='h1'>All Nakuru Parish Churches</h2>
+          <h5>Arch Bishop</h5>
+          <h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h6>
+        </div>
+      </div>
+      <br></br>
+      <div className='one'>
+        <Card style={{ width: '25rem', backgroundColor: 'rgb(247, 241, 235)', borderRadius: '20px' }}>
+          <Card.Img variant="top" src="https://scontent.fnbo1-1.fna.fbcdn.net/v/t1.18169-9/24312564_1559600020752205_5539193794768128898_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=cdbe9c&_nc_ohc=S0p_-3oGfwgAX-LCICD&_nc_ht=scontent.fnbo1-1.fna&oh=00_AfBH5Ix9ZOvcpZZelKaWbayUg8WMEcs8ktlqTrmmOGtjtA&oe=6426571A" />
+          <Card.Body>
+            <Card.Title>Bishop Gichia</Card.Title>
+            <Card.Text>
+              Thika Diocese
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <div className='text1'>
+          <h2 className='h1'>All Nakuru Parish Churches</h2>
+          <h5>Arch Bishop</h5>
+          <h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h6>
+        </div>
+      </div>
+      <br></br>
+      <div className='one'>
+        <Card style={{ width: '25rem', backgroundColor: 'rgb(247, 241, 235)', borderRadius: '20px'}}>
+          <Card.Img variant="top" src="https://scontent.fnbo1-1.fna.fbcdn.net/v/t31.18172-8/1077711_557396390972578_2068465543_o.jpg?_nc_cat=108&ccb=1-7&_nc_sid=9267fe&_nc_ohc=ODYOMiRx0cUAX_bwiky&_nc_ht=scontent.fnbo1-1.fna&oh=00_AfADO0QS1dZLm0qOdvvaUfEGBRBLtKT_DaEPRMswzmrSZQ&oe=642678A9" />
+          <Card.Body>
+            <Card.Title>Bishop Amos</Card.Title>
+            <Card.Text>
+              Nairobi Diocese
+            </Card.Text>
+          </Card.Body>
+        </Card>
+
+        <div className='text1'>
+          <h2 className='h1'>All Nakuru Parish Churches</h2>
+          <h5>Arch Bishop</h5>
+          <h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h6>
+        </div>
+      </div>
+      <br></br>
+      <div className='one'>
+        <Card style={{ width: '25rem', backgroundColor: 'rgb(247, 241, 235)', borderRadius: '20px' }}>
+          <Card.Img variant="top" style={{ height: "300px" }} src="https://scontent.fnbo1-1.fna.fbcdn.net/v/t31.18172-8/24958979_1559601547418719_6213482115742618006_o.jpg?_nc_cat=110&ccb=1-7&_nc_sid=cdbe9c&_nc_ohc=DyYO01U8g_AAX9B3AhJ&_nc_ht=scontent.fnbo1-1.fna&oh=00_AfA5p-dD7nvHuVEhp52M9Yps7hQQm9_lwtsfL739v45zdQ&oe=64267CD1" />
+          <Card.Body>
+            <Card.Title>Bishop Njuguna</Card.Title>
+            <Card.Text>
+              Laikipia Diocese
+            </Card.Text>
+          </Card.Body>
+        </Card>
+
+        <div className='text1'>
+          <h2 className='h1'>All Nakuru Parish Churches</h2>
+          <h5>Arch Bishop</h5>
+          <h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h6>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default BasicExample;
