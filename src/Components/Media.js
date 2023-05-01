5a0c7d4ff217ebe33c08bad138cf14485861ae27
ClementNjeru <njeruclement36@gmail.com>
2023-05-01 23:03:15 +0300

    updates the server db.json

--- src/Components/Media.js
+++ src/Components/Media.js
@@ -1,9 +1,49 @@
-import React from 'react'
+import {useState, useEffect } from 'react';
 
-function Media() {
+function MyComponent() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/data')
+      .then(response => response.json())
+      .then(data => {
+        setData(data);
+        setLoading(false);
+      })
+      .catch(error => {
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
+
   return (
-    <div>Media</div>
-  )
+    <div>
+      {
+        data.map((data) => (
+        <div key={data.id}>
+          <ul key="{name}">{data.name}</ul>
+          <ul key="{age}">{data.age}</ul>
+          <ul key="{email}">{data.email}</ul>
+          <h1> {}</h1>
+        </div>
+        ))
+       
+      }
+      
+      
+      
+    </div>
+  );
 }
 
-export default Media
+export default MyComponent;
