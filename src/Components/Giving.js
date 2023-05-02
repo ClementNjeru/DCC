f9d7c496daafa6f91d5bae712d97d07b54ea547e
ClementNjeru <njeruclement36@gmail.com>
2023-05-02 22:05:20 +0300

    updates the giving page

--- src/Components/Giving.js
+++ src/Components/Giving.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-function Giving() {
+const DonationForm = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [amount, setAmount] = useState(0);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Code to handle form submission
+  };
+
   return (
-    <div>Giving</div>
-  )
-}
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="amount">Amount:</label>
+        <input
+          type="number"
+          id="amount"
+          min="0"
+          step="any"
+          value={amount}
+          onChange={(event) => setAmount(event.target.value)}
+          required
+        />
+      </div>
+      <button type="submit">Donate</button>
+    </form>
+  );
+};
 
-export default Giving
+export default DonationForm;
