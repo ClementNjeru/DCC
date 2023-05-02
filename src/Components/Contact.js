d025621c052dfb671c1efc0d79c1331e48e318df
ClementNjeru <njeruclement36@gmail.com>
2023-05-02 21:42:29 +0300

    updates the contact page

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-function Contact() {
+function ContactForm() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Name:', name);
+    console.log('Email:', email);
+    console.log('Message:', message);
+    // Add code to submit form data to your backend or API here
+    setName("");
+    setEmail("");
+    setMessage("");
+  };
+
   return (
-    <div>Contact</div>
-  )
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="message">Message:</label>
+        <textarea
+          id="message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        ></textarea>
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
 
-export default Contact
+export default ContactForm;
