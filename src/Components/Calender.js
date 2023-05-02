eb563191cb4e99a0deaf6d399d96be837e693912
ClementNjeru <njeruclement36@gmail.com>
2023-05-02 16:29:11 +0300

    modifies calender

--- src/Components/Calender.js
+++ src/Components/Calender.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
-function Calender() {
+function App() {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
+
+  const tileClassName = ({ date, view }) => {
+    if (view === 'month' && date.getDate() === 23 && date.getMonth() === 7) {
+      return 'important-date';
+    }
+  };
+
   return (
-    <div>Calender</div>
-  )
+    <div className="App1">
+      <h1 className='title'>Calendar with Important Dates</h1>
+      <Calendar
+        value={selectedDate}
+        onChange={handleDateChange}
+        tileClassName={tileClassName}
+      />
+      <h2>Upcoming Important Dates</h2>
+      <ul>
+        <li>August 23: Conference</li>
+        <li>September 30: My Birthday</li>
+        <li>October 20: Mashujaa Day</li>
+      </ul>
+    </div>
+  );
 }
 
-export default Calender
+export default App;
--- src/Components/Media.js
+++ src/Components/Media.js
@@ -1,49 +1,67 @@
-import {useState, useEffect } from 'react';
+// import {useState, useEffect } from 'react';
+// import db from './db.json';
 
-function MyComponent() {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+// function MyComponent() {
+//   const [data, setData] = useState([]);
+//   const [loading, setLoading] = useState(true);
+//   const [error, setError] = useState(null);
+
+//   useEffect(() => {
+//     fetch('https://jsonplaceholder.typicode.com/photos')
+//       .then(response => response.json())
+//       .then(data => {
+//         setData(data);
+//         setLoading(false);
+//       })
+//       .catch(error => {
+//         setError(error);
+//         setLoading(false);
+//       });
+//   }, []);
+
+//   if (loading) {
+//     return <p>Loading...</p>;
+//   }
+
+//   if (error) {
+//     return <p>Error: {error.message}</p>;
+//   }
+
+//   return (
+//     <div>
+//       {db.images.map((image) => (
+//         <figure key={image.id}>
+//           <img src={image.src} alt={image.alt} />
+//           <figcaption>{image.caption}</figcaption>
+//         </figure>
+//       ))}
+//     </div>
+//   );
+// }
+
+// export default MyComponent;
+
+
+
+
+import React, { useState, useEffect } from 'react';
+
+function Media() {
+  const [images, setImages] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:3000/data')
+    fetch('http://localhost:3000/images')
       .then(response => response.json())
-      .then(data => {
-        setData(data);
-        setLoading(false);
-      })
-      .catch(error => {
-        setError(error);
-        setLoading(false);
-      });
+      .then(data => setImages(data));
   }, []);
 
-  if (loading) {
-    return <p>Loading...</p>;
-  }
-
-  if (error) {
-    return <p>Error: {error.message}</p>;
-  }
-
   return (
-    <div>
-      {
-        data.map((data) => (
-        <div key={data.id}>
-          <ul key="{name}">{data.name}</ul>
-          <ul key="{age}">{data.age}</ul>
-          <ul key="{email}">{data.email}</ul>
-          <h1> {}</h1>
-        </div>
-        ))
-       
-      }
-      
-      
-      
+    <div className="image-container">
+      {images.map((image, index) => (
+        <img key={index} src={image.src} alt={image.alt} />
+      ))}
     </div>
   );
 }
 
-export default MyComponent;
+export default Media;
